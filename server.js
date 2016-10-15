da84b7493878de177ce2af09436b87d2d51c73d6
rahmona-henry <rahmona.henry@icloud.com>
2016-10-15 15:30:33 +1300

    Add client directory with index.html setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('client'))
+app.set('client',path.join(__dirname,'client'))
+
 app.get ('/', function (req,res){
   res.send('index.html')
 })
